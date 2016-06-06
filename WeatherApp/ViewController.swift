ac6b0b304ca83a91f9826d6574b1ff13d9371def
Peyman Attarzadeh <peymanium@Peymans-Macbook.local>
2016-06-06 18:05:24 +0400

    View controls added

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LBL_City : UILabel!
+    @IBOutlet weak var IMG_Weather : UIImageView!
+    @IBOutlet weak var LBL_Weather : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
